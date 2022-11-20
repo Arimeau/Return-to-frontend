845f4c2559705df87b9b8b7733d612d42dc615f8
proskifcha <115305255+proskifcha@users.noreply.github.com>
2022-11-20 21:03:22 +0300

    Delete Python education directory

--- Python education/practice-7/pr7-1.py
+++
@@ -1,2 +0,0 @@
-r, g, b = [int(i) for i in input().split()]
-print(255 - r, 255 - g, 255 - b)
--- Python education/task-2/task1.py
+++
@@ -1,5 +0,0 @@
-n = int(input('Введите значение n: '))
-res = []
-for i in range(1, n+1):
-    res.append(list(range(1, n+1)))
-print(res)
